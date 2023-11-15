5d1576dbf55d3f4c6f9f170d8e7392897ef4b77b
Kayla Dale <kmeyers119@gmail.com>
2023-11-14 20:55:45 -0600

    changed array & added another for loop

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let friends = ['Bob', 'John', 'Mary', 'Joe', 'David'];
 
-let fLen = friends.length
 for ( let i = 0; i < friends.length; i++) {
-    console.log("hello " + friends);
+    console.log("Hello " + friends[i]);
+}
+
+friends[0] = 'Kevin';
+friends[1] = 'Linda';
+friends[2] = 'Kasie';
+
+for ( let i = 0; i < friends.length; i++) {
+    console.log("Hello " + friends[i]);
 }
